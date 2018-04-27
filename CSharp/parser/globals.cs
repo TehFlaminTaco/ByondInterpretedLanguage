c23cee375069013eb1bd0be1ad00a9150fddc753
William Lemon <William.Lemon2@gmail.com>
2018-04-27 12:17:50 +1000

    Added string gmatch and gsub
    Like lua, but Regex.

--- CSharp/parser/globals.cs
+++ CSharp/parser/globals.cs
@@ -207,6 +207,91 @@ namespace ByondLang{
                 }
             });
 
+            str["gmatch"] = new VarFunction(delegate(Scope scope, Dictionary<int, Var> returnTarget, int returnID, VarList arguments){
+                if(arguments.number_vars.ContainsKey(0) && arguments.number_vars[0] is VarString && arguments.number_vars.ContainsKey(1) && arguments.number_vars[1] is VarString){
+                    string haystack = (string)(VarString)arguments.number_vars[0];
+                    string needle = (string)(VarString)arguments.number_vars[1];
+                    Regex matcher = new Regex(needle, RegexOptions.None, new System.TimeSpan(0, 0, 1));
+                    try{
+                        MatchCollection M = matcher.Matches(haystack);
+                        int i = 0;
+                        returnTarget[returnID] = new VarFunction(delegate(Scope nscope, Dictionary<int, Var> nreturnTarget, int nreturnID, VarList narguments){
+                            if(i < M.Count){
+                                Match m = M[i];
+                                if(m.Groups.Count == 1){
+                                    nreturnTarget[nreturnID] = m.Groups[0].Value;
+                                }else{
+                                    VarList outList = new VarList();
+                                    for(int c=0; c < m.Groups.Count; c++){
+                                        outList.string_vars[m.Groups[c].Name] = outList.number_vars[c] = m.Groups[c].Value;
+                                    }
+                                    nreturnTarget[nreturnID] = outList;
+                                }
+                            }else{
+                                nreturnTarget[nreturnID] = Var.nil;
+                            }
+                            i++;
+                        });
+                    }catch{}
+                }
+            });
+
+            str["gsub"] = new VarFunction(delegate(Scope scope, Dictionary<int, Var> returnTarget, int returnID, VarList arguments){
+                if(arguments.number_vars.ContainsKey(0) && arguments.number_vars[0] is VarString && arguments.number_vars.ContainsKey(1) && arguments.number_vars[1] is VarString && arguments.number_vars.ContainsKey(2) && arguments.number_vars[2] is VarString){
+                    string haystack = (string)(VarString)arguments.number_vars[0];
+                    string needle = (string)(VarString)arguments.number_vars[1];
+                    Regex matcher = new Regex(needle, RegexOptions.None, new System.TimeSpan(0, 0, 1));
+                    try{
+                        returnTarget[returnID] = matcher.Replace(haystack, (string)(VarString)arguments.number_vars[2]);
+                    }catch{}
+                }else if(arguments.number_vars.ContainsKey(0) && arguments.number_vars[0] is VarString && arguments.number_vars.ContainsKey(1) && arguments.number_vars[1] is VarString && arguments.number_vars.ContainsKey(2) && arguments.number_vars[2] is VarFunction){
+                    string haystack = (string)(VarString)arguments.number_vars[0];
+                    string needle = (string)(VarString)arguments.number_vars[1];
+                    VarFunction replace_func = (VarFunction)arguments.number_vars[2];
+                    Regex matcher = new Regex(needle, RegexOptions.None, new System.TimeSpan(0, 0, 1));
+                    try{
+                        MatchCollection M = matcher.Matches(haystack);
+                        string built = "";
+                        int lastIndex = 0;
+                        int i = 0;
+                        DoLater loopfnc = null;
+                        State s_state = new State();
+                        loopfnc = new DoLater(delegate{
+                            if(i < M.Count){
+                                Match m = M[i];
+                                if(i > 0){
+                                    if(s_state.returns[0] is VarString){
+                                        built += (string)(VarString)s_state.returns[0];
+                                    }
+                                }
+                                built += haystack.Substring(lastIndex, m.Index - lastIndex);
+                                lastIndex = m.Index + m.Groups[0].Length;
+                                if(m.Groups.Count == 1){
+                                    replace_func.Call(scope, s_state.returns, 0, m.Groups[0].Value);
+                                }else{
+                                    VarList outList = new VarList();
+                                    for(int c=0; c < m.Groups.Count; c++){
+                                        outList.string_vars[m.Groups[c].Name] = outList.number_vars[c] = m.Groups[c].Value;
+                                    }
+                                    replace_func.Call(scope, s_state.returns, 0, outList);
+                                }
+                                i++;
+                                scope.callstack.Push(loopfnc);
+                            }else{
+                                if(i > 0){
+                                    if(s_state.returns[0] is VarString){
+                                        built += (string)(VarString)s_state.returns[0];
+                                    }
+                                }
+                                built += haystack.Substring(lastIndex, haystack.Length - lastIndex);
+                                returnTarget[returnID] = built;
+                            }
+                        });
+                        scope.callstack.Push(loopfnc);
+                    }catch{}
+                }
+            });            
+
 
             return string_VAR;
         }
